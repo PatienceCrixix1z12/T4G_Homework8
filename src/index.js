81459ecf7bc760eafc132e737c9a2745f922d1fa
PatienceCrixix1z12 <nutsugahamapatience12@gmail.com>
2024-12-04 09:52:03 +0000

    add all files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function checkGender(){
+let gender =prompt("What is your gender please?");
+if (gender =="female"){
+    alert(`You are a girl`);
+}
+else if(gender== "male"){
+    alert(`You are a boy`)
+}
+}
+checkGender()
